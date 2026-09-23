b30c2d9794daf8ae82f9be39f438761c1d0aa84d
Laxman M <laxmanm1309@gmail.com>
2026-09-23 11:27:15 +0530

    Add Medium Java Solution for 1658. Minimum Operations to Reduce X to Zero from LeetCode

--- LeetCode/Patterns/Sliding Window/1658. Minimum Operations to Reduce X to Zero/Solution.java
+++ LeetCode/Patterns/Sliding Window/1658. Minimum Operations to Reduce X to Zero/Solution.java
@@ -1,28 +1,24 @@
 class Solution {
+
     public int minOperations(int[] nums, int x) {
-        int n = nums.length;
-        if(nums[0] > x && nums[n-1] > x)    return -1;
-        int count = 0;
-        int l = 0;
-        int r = n-1;
-        // for(int i = 0 ; i < n ; i++){
-        while(l <= r){
-            if(x == 0)  return count;
-            else{
-                if(nums[l] <= x && nums[l] >= nums[r]){
-                    x -= nums[l];
-                    l++;
-                    count++;
-                }
-                else if(nums[r] <= x){
-                    x -= nums[r];
-                    r--;
-                    count++;
-                }
-                else    return -1;
-            }
-        }
-        System.out.println(count +" hi "+x);
-        return (x == 0) ? count : -1;
+        int ans = solve(nums, 0, nums.length - 1, x);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+
+    static int solve(int[] nums, int l, int r, int x) {
+
+        if (x == 0) return 0;
+
+        if (l > r || x < 0) return Integer.MAX_VALUE;
+
+        int left = solve(nums, l + 1, r, x - nums[l]);
+
+        int right = solve(nums, l, r - 1, x - nums[r]);
+
+        int best = Math.min(left, right);
+
+        return best == Integer.MAX_VALUE
+                ? best
+                : best + 1;
     }
 }
