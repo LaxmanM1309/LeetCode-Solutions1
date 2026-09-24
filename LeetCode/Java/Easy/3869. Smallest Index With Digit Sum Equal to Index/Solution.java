cab42bc3897db560d17d4c8fa630d9646c3de70f
Laxman M <laxmanm1309@gmail.com>
2026-09-24 09:20:50 +0530

    Add Easy Java Solution for 3869. Smallest Index With Digit Sum Equal to Index from LeetCode

--- LeetCode/Java/Easy/3869. Smallest Index With Digit Sum Equal to Index/Solution.java
+++ LeetCode/Java/Easy/3869. Smallest Index With Digit Sum Equal to Index/Solution.java
@@ -1,20 +1,27 @@
 class Solution {
+    public static int digitSum(int num){
+        int sum = 0;
+        while(num > 0){
+            int temp = num % 10;
+            num /= 10;
+            sum += temp;
+        }
+        return sum;
+    }
     public int smallestIndex(int[] nums) {
-        // Arrays.sort(nums);
-        int ans = Integer.MAX_VALUE;
         for(int i = 0 ; i < nums.length ; i++){
-            int number = nums[i];
-            int sum = 0;
-            while(number > 0){
-                int temp = number % 10;
-                number /= 10;
-                sum += temp;
-            }
-            if(i == sum){
-                ans = Math.min(ans , sum);
+            int sum = digitSum(nums[i]);
+            if(sum == i){
+                return sum;
             }
         }
-        return (ans != Integer.MAX_VALUE)?
-                ans : -1;
+        return -1;
     }
 }
+
+
+
+
+
+
+
